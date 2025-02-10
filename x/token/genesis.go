f8e60e47c7b051815b53837f6b1a9d1e1df2d341
forbesus <ogawakameto@gmail.com>
2025-02-10 21:25:24 +0000

    fix: wallet address

--- x/token/genesis.go
+++ x/token/genesis.go
@@ -25,16 +25,16 @@ func DefaultGenesis() *GenesisState {
 			{Denom: "smartTM", TotalSupply: 100000}, // Set total supply
 		},
 		Balances: []Balance{
-			{Address: "wallet1_address", Amount: 10000},
-			{Address: "wallet2_address", Amount: 10000},
-			{Address: "wallet3_address", Amount: 10000},
-			{Address: "wallet4_address", Amount: 10000},
-			{Address: "wallet5_address", Amount: 10000},
-			{Address: "wallet6_address", Amount: 10000},
-			{Address: "wallet7_address", Amount: 10000},
-			{Address: "wallet8_address", Amount: 10000},
-			{Address: "wallet9_address", Amount: 10000},
-			{Address: "wallet10_address", Amount: 10000},
+			{Address: "cosmos1q7ev25sze0g6pua0sdvqpnkjge86f6elpqyxmn", Amount: 10000},
+			{Address: "cosmos1ml95mze9j50nre38w7aqkatssq3fxyuc3y3j2y", Amount: 10000},
+			{Address: "cosmos1cxq2ynzy7y96s8thpggfnupr2c4yesjclszteu", Amount: 10000},
+			{Address: "cosmos1cq9s4kyrepxxy3vp7c6uqzypclvrwtx75atysu", Amount: 10000},
+			{Address: "cosmos1e9r2fhg234xvh8fywfh4g5ygr6g7jrkm3ut792", Amount: 10000},
+			{Address: "cosmos1ardueyuwhwuxm0x4gn7tvrukv8w6jx200y08k4", Amount: 10000},
+			{Address: "cosmos1zhfk7pf6q7zrdzmtk5kfjgkjpacnyc6cfwesjc", Amount: 10000},
+			{Address: "cosmos19cc5agyhh40cp4vzm0wqek8xhe9nf0sxvyc2tn", Amount: 10000},
+			{Address: "cosmos1vafkych49fzcqqjxz90tp3km38zy8h5hmsgwpg", Amount: 10000},
+			{Address: "cosmos1qrd52sxtj9aywle7n8vaethnyjy4typu0p4gj0", Amount: 10000},
 		},
 	}
 }
